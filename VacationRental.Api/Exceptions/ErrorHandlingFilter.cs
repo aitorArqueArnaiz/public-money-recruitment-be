e8bf23ce47828bbc49b5779697a7f5956c39cab9
aitoredualdnun <aarque@userzoom.com>
2023-01-28 09:47:31 +0100

    Applying the refactor for making the code more readeable

---
+++ VacationRental.Api/Exceptions/ErrorHandlingFilter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog;
+
+public class ErrorHandlingFilter : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        var exception = context.Exception;
+        Log.ForContext<ErrorHandlingFilter>()
+            .Error($"Error ocurred during execution of action {context.ActionDescriptor.DisplayName}. Error message {exception}");
+
+        context.ExceptionHandled = true;
+    }
+}
